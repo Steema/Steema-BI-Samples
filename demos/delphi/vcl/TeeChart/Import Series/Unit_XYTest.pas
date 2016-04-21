1206d731e147855556d3990bab1c24da9318eaf3
David Berneda <david@steema.com>
2016-04-21 17:57:28 +0200

    New small test of converting TeeChart Series to / from TDataItem

---
+++ demos/delphi/vcl/TeeChart/Import Series/Unit_XYTest.pas
@@ -0,0 +1,60 @@
+unit Unit_XYTest;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, VclTee.TeeGDIPlus, VCLTee.TeEngine,
+  VCLTee.Series, BI.VCL.Grid, VCLTee.TeeTools, Vcl.ExtCtrls, VCLTee.TeeProcs,
+  VCLTee.Chart, BI.VCL.Chart;
+
+type
+  TFormXYTest = class(TForm)
+    BIGrid1: TBIGrid;
+    Panel1: TPanel;
+    Splitter1: TSplitter;
+    BIChart1: TBIChart;
+    Series1: TPointSeries;
+    Chart1: TChart;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFormXYTest.FormCreate(Sender: TObject);
+var t : Integer;
+begin
+  // Fill series with random XY values
+
+  Series1.Clear;
+
+  Series1.XValues.Order:=TChartListOrder.loNone;
+
+  for t:=1 to 100 do
+      Series1.AddXY(Random(1000),Random(1000));
+
+  // Convert Series1 to a TDataItem
+  BIGrid1.Data:=TChartData.From(Series1);
+
+  // Convert a TDataItem to a new Series
+  Chart1.AddSeries(TChartData.From(BIGrid1.Data, Self, TPointSeries));
+
+  // Cosmetic settings:
+  (Chart1[0] as TPointSeries).Pointer.Style:=psCircle;
+  Chart1[0].ColorEachPoint:=True;
+  Chart1.View3D:=False;
+end;
+
+procedure TFormXYTest.FormDestroy(Sender: TObject);
+begin
+  BIGrid1.Data.Free;
+end;
+
+end.
