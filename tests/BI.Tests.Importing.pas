4a4d191c29adf51de2bc8b337128c7df5a53f824
David Berneda <david@steema.com>
2016-10-11 15:07:22 +0200

    Improved "Importing" test with options (conditional defines) to use alternative 3rd party "engines" (OXml, Omni XML, JSON SuperObject)

--- tests/BI.Tests.Importing.pas
+++ tests/BI.Tests.Importing.pas
@@ -6,16 +6,45 @@
 {*********************************************}
 unit BI.Tests.Importing;
 
+{
+  Several tests about importing data into TDataItem, from different formats
+}
+
+// Remove "." to run tests using a different 3rd party XML engine than MSXML:
+{.$DEFINE USE_OXML}
+{.$DEFINE USE_OMNI}
+
+// Remove "." to run tests using a different 3rd party JSON engine than System.JSON:
+{.$DEFINE USE_SUPEROBJECT}
+
 interface
 
 uses
   BI.Arrays, BI.Data, DUnitX.TestFramework,
-  BI.Data.CSV, BI.Data.XML, BI.Data.ClientDataset, BI.Data.JSON;
+
+  {$IFDEF USE_OXML}
+  BI.Data.XML.OXml,
+  {$ELSE}
+  {$IFDEF USE_OMNI}
+  BI.Data.XML.Omni,
+  {$ENDIF}
+  {$ENDIF}
+
+  {$IFDEF USE_SUPEROBJECT}
+  BI.Data.JSON.SuperObject,
+  {$ENDIF}
+
+  BI.Data.CSV, BI.Data.XML, BI.Data.ClientDataset, BI.Data.JSON, BI.Data.HTML;
 
 type
   [TestFixture]
   TImporting_Test=class(TObject)
   strict private
+
+    procedure AssertAB(const AData:TDataItem);
+    procedure Assert123(const AData:TDataItem; const Inverted:Boolean=False);
+
+    function FindHTMLTestFile:String;
   public
     [Setup]
     procedure Setup;
@@ -28,28 +57,66 @@ type
     procedure CSVHeader;
     [Test]
     procedure CSVTabs;
+
+    [Test]
+    procedure JSON;
+
+    {$IFDEF USE_OXML} // <-- Pending problem, standard MSXML raises exception
+    [Test]
+    {$ELSE}
+    {$IFDEF USE_OMNI}
+    [Test]
+    {$ENDIF}
+    {$ENDIF}
+    procedure XML;
+
+    {$IFDEF USE_OXML} // <-- Pending problem, standard MSXML raises exception
+    [Test]
+    {$ELSE}
+    {$IFDEF USE_OMNI}
+    [Test]
+    {$ENDIF}
+    {$ENDIF}
+    procedure HTML;
   end;
 
 implementation
 
+uses
+  System.SysUtils, System.IOUtils;
+
 { TImporting_Test }
+
+procedure TImporting_Test.Assert123(const AData:TDataItem; const Inverted:Boolean=False);
+begin
+  Assert.IsNotNull(AData);
+  Assert.AreEqual<Int64>(AData.Count,1);
+  Assert.AreEqual(AData.Items.Count,3);
+
+  Assert.AreEqual<TDataKind>(AData[0].Kind,TDataKind.dkInt32);
+  Assert.AreEqual<TDataKind>(AData[1].Kind,TDataKind.dkInt32);
+  Assert.AreEqual<TDataKind>(AData[2].Kind,TDataKind.dkInt32);
+
+  if Inverted then
+  begin
+    Assert.AreEqual(AData[0].Int32Data[0],3);
+    Assert.AreEqual(AData[1].Int32Data[0],2);
+    Assert.AreEqual(AData[2].Int32Data[0],1);
+  end
+  else
+  begin
+    Assert.AreEqual(AData[0].Int32Data[0],1);
+    Assert.AreEqual(AData[1].Int32Data[0],2);
+    Assert.AreEqual(AData[2].Int32Data[0],3);
+  end;
+end;
 
 procedure TImporting_Test.CSV;
 var Data : TDataItem;
 begin
   Data:=TBICSV.FromText('1,2,3');
   try
-    Assert.IsNotNull(Data);
-    Assert.AreEqual<Int64>(Data.Count,1);
-    Assert.AreEqual(Data.Items.Count,3);
-
-    Assert.AreEqual<TDataKind>(Data[0].Kind,TDataKind.dkInt32);
-    Assert.AreEqual<TDataKind>(Data[1].Kind,TDataKind.dkInt32);
-    Assert.AreEqual<TDataKind>(Data[2].Kind,TDataKind.dkInt32);
-
-    Assert.AreEqual(Data[0].Int32Data[0],1);
-    Assert.AreEqual(Data[1].Int32Data[0],2);
-    Assert.AreEqual(Data[2].Int32Data[0],3);
+    Assert123(Data);
   finally
     Data.Free;
   end;
@@ -80,6 +147,19 @@ begin
   end;
 end;
 
+procedure TImporting_Test.AssertAB(const AData:TDataItem);
+begin
+  Assert.IsNotNull(AData);
+  Assert.AreEqual<Int64>(AData.Count,1);
+  Assert.AreEqual(AData.Items.Count,2);
+
+  Assert.AreEqual<TDataKind>(AData[0].Kind,TDataKind.dkText);
+  Assert.AreEqual<TDataKind>(AData[1].Kind,TDataKind.dkText);
+
+  Assert.AreEqual(AData[0].TextData[0],'A');
+  Assert.AreEqual(AData[1].TextData[0],'B');
+end;
+
 const
   Tab=#9;
 
@@ -93,15 +173,7 @@ begin
 
     Data:=CSV.ImportText('"A"'+Tab+'"B"');
     try
-      Assert.IsNotNull(Data);
-      Assert.AreEqual<Int64>(Data.Count,1);
-      Assert.AreEqual(Data.Items.Count,2);
-
-      Assert.AreEqual<TDataKind>(Data[0].Kind,TDataKind.dkText);
-      Assert.AreEqual<TDataKind>(Data[1].Kind,TDataKind.dkText);
-
-      Assert.AreEqual(Data[0].TextData[0],'A');
-      Assert.AreEqual(Data[1].TextData[0],'B');
+      AssertAB(Data);
     finally
       Data.Free;
     end;
@@ -110,14 +182,84 @@ begin
   end;
 end;
 
+function TImporting_Test.FindHTMLTestFile:String;
+const
+  HTMLFile='BI.Test.HTML.html';
+begin
+  result:=HTMLFile;
+
+  if not TFile.Exists(result) then
+  begin
+    result:=TPath.Combine('.\..',result);
+
+    if not TFile.Exists(result) then
+    begin
+      result:=TPath.Combine('.\..',result);
+
+      Assert.IsTrue(TFile.Exists(result),'File: '+HTMLFile+' cannot be found');
+    end;
+  end;
+end;
+
+procedure TImporting_Test.HTML;
+var Data : TDataItem;
+begin
+  Data:=TBIHTML.FromFile(FindHTMLTestFile);
+  try
+    Assert.IsNotNull(Data);
+    Assert.AreEqual<Int64>(Data.Items.Count,15);
+  finally
+    Data.Free;
+  end;
+end;
+
+procedure TImporting_Test.JSON;
+var Data : TDataItem;
+begin
+  Data:=TBIJSON.FromText('{"1":1, "2":2, "3":3}');
+  try
+    Assert123(Data {$IFDEF USE_SUPEROBJECT},True{$ENDIF});
+  finally
+    Data.Free;
+  end;
+end;
+
 procedure TImporting_Test.Setup;
 begin
+  // When using alternative 3rd party "engines", initialize them:
 
+  {$IFDEF USE_OXML}
+  TBIXML.EngineClass:=TOXml;
+  {$ELSE}
+  {$IFDEF USE_OMNI}
+  TBIXML.EngineClass:=TOmniXML;
+  {$ENDIF}
+  {$ENDIF}
+
+  {$IFDEF USE_SUPEROBJECT}
+  TBIJSON.EngineClass:=TSuperObjectJSON;
+  {$ENDIF}
 end;
 
 procedure TImporting_Test.TearDown;
 begin
+end;
 
+procedure TImporting_Test.XML;
+const
+  XMLHeader='<?xml version="1.0" encoding="UTF-8"?>';
+
+var Data : TDataItem;
+begin
+  Data:=TBIXML.FromText('<sample><a>1</a><b>2</b><c>3</c></sample>');
+  try
+    Assert.IsNotNull(Data);
+    Assert.AreEqual<Int64>(Data.Items.Count,1);
+
+    Assert123(Data['sample']);
+  finally
+    Data.Free;
+  end;
 end;
 
 initialization
