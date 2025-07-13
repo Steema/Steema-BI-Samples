6f12ca4bf47f7bf204ff5a93965c2a8902f786b3
david <davidberneda@gmail.com>
2025-07-13 19:51:58 +0200

    Added support for Google Gemini responses in JSON format

--- sources/BI.AI.pas
+++ sources/BI.AI.pas
@@ -74,7 +74,9 @@ end;
 class function TBIAI.From(const Key, Question, Agent: String): TDataItem;
 
   function GeminiOutput(const JSON:String):String;
-  const csvFlag='```csv'+#10;
+  const
+    csvFlag='```csv'+#10;
+    jsonFlag='```json'+#10;
 
   var tmp,
       tmpContent : TJSONObject;
@@ -83,20 +85,31 @@ class function TBIAI.From(const Key, Question, Agent: String): TDataItem;
   begin
     tmp:=TJSONObject.ParseJSONValue(JSON) as TJSONObject;
 
-    if tmp.TryGetValue('candidates', Arr) and (not Arr.IsEmpty) then
-       if Arr.Items[0].TryGetValue('content', tmpContent) then
-          if tmpContent.TryGetValue('parts', Arr) and (not Arr.IsEmpty) then
-             if Arr.Items[0].TryGetValue('text', tmpS) then
-             begin
-               // Cleanup:
-               if tmpS.StartsWith(CSVFlag) then
-                  tmpS:=tmpS.Substring(CSVFlag.Length);
+    if tmp<>nil then
+    try
+      if tmp.TryGetValue('candidates', Arr) and (not Arr.IsEmpty) then
+         if Arr.Items[0].TryGetValue('content', tmpContent) then
+            if tmpContent.TryGetValue('parts', Arr) and (not Arr.IsEmpty) then
+               if Arr.Items[0].TryGetValue('text', tmpS) then
+               begin
+                 // Cleanup:
+                 if tmpS.StartsWith(CSVFlag) then
+                    tmpS:=tmpS.Substring(CSVFlag.Length)
+                 else
+                 if tmpS.StartsWith(JSONFlag) then
+                    tmpS:=tmpS.Substring(JSONFlag.Length);
 
-               if tmpS.EndsWith('```') then
-                  tmpS:=tmpS.Remove(tmpS.Length-3,3);
+                 if tmpS.EndsWith('```') then
+                    tmpS:=tmpS.Remove(tmpS.Length-3,3)
+                 else
+                 if tmpS.EndsWith('```'#10) then
+                    tmpS:=tmpS.Remove(tmpS.Length-4,4);
 
-               Exit(tmpS);
-             end;
+                 Exit(tmpS);
+               end;
+    finally
+      tmp.Free;
+    end;
 
     DoError('AI Google Gemini error: Wrong response');
   end;
