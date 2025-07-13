fc9d43ca41aac6f7051b35de9abe181d78aeb9ae
david <davidberneda@gmail.com>
2025-07-13 19:52:25 +0200

    New AI example and couple of fixes / workarounds

--- demos/3rd_party/AI/Unit_Main.pas
+++ demos/3rd_party/AI/Unit_Main.pas
@@ -9,7 +9,7 @@ uses
   FMX.Memo, FMXTee.Engine, FMXTee.Procs, FMXTee.Chart, FMXBI.Chart.Plugin,
   FMXBI.Chart, FMXBI.DataControl, FMXBI.Grid, FMX.Layouts,
 
-  BI.DataItem, BI.AI, BI.XMLData, BI.CSV, BI.JSON;
+  BI.DataItem, BI.AI, BI.XMLData, BI.CSV, BI.JSON, FMX.Objects, FMXTee.Tools;
 
 type
   TForm_AI_Demo = class(TForm)
@@ -25,10 +25,16 @@ type
     Label2: TLabel;
     EditAPIKey: TEdit;
     Button1: TButton;
+    Text1: TText;
+    Text2: TText;
+    ComboExample: TComboBox;
     procedure EditAPIKeyChangeTracking(Sender: TObject);
     procedure CBAgentChange(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure Text2Click(Sender: TObject);
+    procedure ComboExampleChange(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
     { Private declarations }
 
@@ -50,17 +56,36 @@ uses
 procedure TForm_AI_Demo.Button1Click(Sender: TObject);
 var Data1 : TDataItem;
 begin
-  // One line of code, call Gemini and get a data structure:
-  Data1:=TBIAI.From(EditAPIKey.Text, Memo1.Text.Trim, TBIAI.GoogleGemini);
+  TeeChangeCursor(Self,crHourGlass);
+  try
+    // One line of code, call Gemini and get a data structure:
+    Data1:=TBIAI.From(EditAPIKey.Text, Memo1.Text.Trim, TBIAI.GoogleGemini);
 
-  // Visualize it:
-  BIGrid1.Data:=Data1;
-  BIChart1.Data:=Data1;
+    // Visualize it:
+    BIGrid1.Data:=Data1;
+    BIChart1.Data:=Data1;
+  finally
+    TeeChangeCursor(Self,crDefault);
+  end;
 end;
 
 procedure TForm_AI_Demo.CBAgentChange(Sender: TObject);
 begin
   EditAPIKey.Text:='';
+end;
+
+procedure TForm_AI_Demo.ComboExampleChange(Sender: TObject);
+const
+  Example1 = 'Give me the list of the highest 10 mountains by elevation in csv format, just the list';
+  Example2 = 'I want a list of car sales worldwide this year, by month and by brand (top 10 brands), in csv format, just the list';
+
+begin
+  case ComboExample.ItemIndex of
+    0: Memo1.Text:=Example1;
+    1: Memo1.Text:=Example2;
+  else
+    Memo1.Text:='';
+  end;
 end;
 
 procedure TForm_AI_Demo.EditAPIKeyChangeTracking(Sender: TObject);
@@ -71,6 +96,13 @@ begin
 end;
 
 // Just a handy trick, put your key on this file to avoid always typing it:
+procedure TForm_AI_Demo.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  BIGrid1.Data:=nil; // <-- just to skip a FMX probable AV bug at TModel Cache
+
+  BIChart1.Data.Free;
+end;
+
 procedure TForm_AI_Demo.FormCreate(Sender: TObject);
 var KeyFile : String;
 begin
@@ -78,6 +110,11 @@ begin
 
   if TFile.Exists(KeyFile) then
      EditAPIKey.Text:=TFile.ReadAllText(KeyFile);
+end;
+
+procedure TForm_AI_Demo.Text2Click(Sender: TObject);
+begin
+  TeeGotoURL(0,Text2.Text);
 end;
 
 end.
